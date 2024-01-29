c49fc5e3825e0501b2ed71bda9274101381cbff2
Kiselevdmitrij <157107535+Kiselevdmitrij@users.noreply.github.com>
2024-01-29 12:53:25 +0300

    Update Program.cs

--- ConsoleApp1/task 5/Program.cs
+++ ConsoleApp1/task 5/Program.cs
@@ -11,11 +11,11 @@ namespace task_5
         static void Main(string[] args)
         {
             double a = 0.1;
-            double b = 0.1;
+            double b = 0.2;
             int x = 1;
             double tt = 1 / 5.0;
             double r = Math.Pow((Math.Pow(x, 2) + b), tt) - (Math.Pow(b, 2) * Math.Pow(Math.Sin(x + a), 3))/x;
-            Console.WriteLine(r);
+            Console.WriteLine(Math.Round(r,4));
             Console.ReadKey();
         }
     }
